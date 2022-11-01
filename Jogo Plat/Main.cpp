17e25ebc169a8df26223f040dd5ece9b3d022446
Miguel Brack <102604217+Miguel3074@users.noreply.github.com>
2022-11-01 00:08:09 -0300

    Add files via upload

--- Jogo Plat/Main.cpp
+++ Jogo Plat/Main.cpp
@@ -1,7 +1,7 @@
 #include "stdafx.h"
 #include "Jogo.h"
 #include "menu.h"
-/*
+
 int main()
 {
     //init srand
@@ -16,34 +16,7 @@ int main()
     delete menu;
 
     menu = nullptr;
-    //loop do jogo
 
     //fim
     return EXIT_SUCCESS;
-}*/
-
-int main()
-{
-    //init srand
-    srand(static_cast<unsigned>(time(NULL)));
-
-    //começa a classe jogo
-
-    Jogo jogo;
-
-    
-    //loop do jogo
-
-    while (jogo.rodando())
-    {
-        //update
-
-        jogo.update();
-        //render
-
-        jogo.render();
-    }
-
-    //fim
-    return EXIT_SUCCESS; 
 }
--- Jogo Plat/Menu.cpp
+++ Jogo Plat/Menu.cpp
@@ -2,10 +2,11 @@
 
 void Menu::set_values()
 {
+	//inicializa todas variaveis
+
 	this->janela->create(VideoMode(1920, 1080), "Menu JOGO", Style::Titlebar | Style::Close);
 	this->janela->setPosition(Vector2i(0, 0));
 
-	this->posi = 0;
 	this->pressed = this->theselect = false;
 
 	this->font->loadFromFile("C:/Users/migue/OneDrive/Documentos/FACUL/programação/JOGO SIMAO/Font.otf");
@@ -16,10 +17,10 @@ void Menu::set_values()
 	this->posMouse = { 0,0 };
 	this->cordMouse = { 0,0 };
 
-	this->options = { "NOME DO JOGO","Jogar","Opcoes","Sobre","Sair" };
-	this->texts.resize(5);
-	coords = { { 610,250 } , { 610,400 }, { 610,500 }, { 610,600 }, { 610,700 } };
-	sizes = { 100,50,50,50,50 };
+	this->options = { "NOME DO JOGO","Jogar","Fases","Opcoes","Sobre","Sair"};
+	this->texts.resize(6);
+	coords = { { 610,200 } , { 610,350 }, { 610,450 }, { 610,550 }, { 610,650 }, { 610,750 } };
+	sizes = { 100,50,50,50,50,50 };
 
 	for (size_t i{}; i < texts.size(); ++i) {
 		this->texts[i].setFont(*font);
@@ -28,6 +29,20 @@ void Menu::set_values()
 		this->texts[i].setOutlineColor(Color::Black);
 		this->texts[i].setPosition(coords[i]);
 	}
+
+	this->texts[1].setOutlineThickness(4);
+	posi = 1;
+
+	this->play->setSize(Vector2f(110, 50));
+	this->play->setPosition(610, 350);
+	this->fases->setSize(Vector2f(120, 50));
+	this->fases->setPosition(610, 450);
+	this->opcoes->setSize(Vector2f(130, 50));
+	this->opcoes->setPosition(610, 550);
+	this->sobre->setSize(Vector2f(120, 50));
+	this->sobre->setPosition(610, 650);
+	this->sair->setSize(Vector2f(100, 50));
+	this->sair->setPosition(610, 750);
 }
 
 void Menu::loop_events()
@@ -37,6 +52,131 @@ void Menu::loop_events()
 		if (ev.type == Event::Closed) {
 
 			this->janela->close();
+		}
+
+		posMouse = Mouse::getPosition(*janela);
+		cordMouse = janela->mapPixelToCoords(posMouse);
+
+		//Seleciona as opcoes usando seta para cima e para baixo
+
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)&& !pressed) {
+			if (posi < 5) {
+				++posi;
+				this->pressed = true;
+				this->texts[posi].setOutlineThickness(4);
+				this->texts[posi - 1].setOutlineThickness(0);
+				this->pressed = false;
+				this->theselect = false;
+			}
+		}
+		if ((sf::Keyboard::isKeyPressed(sf::Keyboard::Up) ) && !pressed) {
+			if (posi > 1) {
+				--posi;
+				this->pressed = true;
+				this->texts[posi].setOutlineThickness(4);
+				this->texts[posi + 1].setOutlineThickness(0);
+				this->pressed = false;
+				this->theselect = false;
+			}
+		}
+
+		//Abre a opcao selecionada com o enter
+
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Enter)&& !theselect) {
+			this->theselect = true;
+
+			//realizará as acoes de cada opcao
+
+			if (posi == 1) { //RODA JOGO
+
+				this->janela->close();
+
+				while (this->jogo.rodando())
+				{
+					//update
+
+					this->jogo.update();
+					//render
+
+					this->jogo.render();
+				}
+
+			}
+			else if (posi == 2) { //COLOCAR AS FASES
+
+			}
+			else if (posi == 3) { //COLOCAR AS OPCOES
+
+			}
+			else if (posi == 4) { //COLOCAR SOBRE
+
+			}
+			else if (posi == 5) { //FECHA JANELA
+				this->janela->close();
+			}
+		}
+
+		//Verifica se o mouse está acima de alguma opçao, se estiver marcara a opçao e verificará se foi realizado o clique com o mouse
+
+		if (this->play->getGlobalBounds().contains(cordMouse)) {
+
+			this->texts[posi].setOutlineThickness(0);
+			this->posi = 1;
+			this->texts[posi].setOutlineThickness(4);
+			if (Mouse::isButtonPressed(Mouse::Left)) {
+
+				this->janela->close();
+
+				while (this->jogo.rodando())
+				{
+					//update
+
+					this->jogo.update();
+					//render
+					this->jogo.render();
+				}
+				
+			}
+		}
+
+		if (this->fases->getGlobalBounds().contains(cordMouse)) {
+
+			this->texts[posi].setOutlineThickness(0);
+			this->posi = 2;
+			this->texts[posi].setOutlineThickness(4);
+			if (Mouse::isButtonPressed(Mouse::Left)) {
+
+			}
+		}
+
+		if (this->opcoes->getGlobalBounds().contains(cordMouse)) {
+
+			this->texts[posi].setOutlineThickness(0);
+			this->posi = 3;
+			this->texts[posi].setOutlineThickness(4);
+			if (Mouse::isButtonPressed(Mouse::Left)) {
+
+			}
+		}
+
+		if (this->sobre->getGlobalBounds().contains(cordMouse)) {
+
+			this->texts[posi].setOutlineThickness(0);
+			this->posi = 4;
+			this->texts[posi].setOutlineThickness(4);
+			if (Mouse::isButtonPressed(Mouse::Left)) {
+
+			}
+		}
+
+		if (this->sair->getGlobalBounds().contains(cordMouse)) {
+
+			this->texts[posi].setOutlineThickness(0);
+			this->posi = 5;
+			this->texts[posi].setOutlineThickness(4);
+			if (Mouse::isButtonPressed(Mouse::Left)) {
+				this->janela->close();
+			}
 		}
 	}
 }
@@ -54,7 +194,11 @@ void Menu::draw_all()
 Menu::Menu()
 {
 	this->janela = new RenderWindow();
-	this->closeJanela = new RectangleShape();
+	this->play = new RectangleShape();
+	this->fases = new RectangleShape();
+	this->opcoes = new RectangleShape();
+	this->sobre = new RectangleShape();
+	this->sair = new RectangleShape();
 	this->font = new Font();
 	this->image = new Texture();
 	this->bg = new Sprite();
@@ -65,7 +209,11 @@ Menu::Menu()
 Menu::~Menu()
 {
 	delete janela;
-	delete closeJanela;
+	delete play;
+	delete fases;
+	delete opcoes;
+	delete sobre;
+	delete sair;
 	delete font;
 	delete image;
 	delete bg;
@@ -76,6 +224,5 @@ void Menu::run_menu()
 	while (this->janela->isOpen()) {
 		loop_events();
 		draw_all();
-
 	}
 }
